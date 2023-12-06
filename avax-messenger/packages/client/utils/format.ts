027b8d0b2e0e2dac774641243f1d4c864e2f9e8c
kaz <knman7@hotmail.com>
2023-12-06 15:19:19 +0900

    Remove ava-messenger

--- avax-messenger/packages/client/utils/format.ts
+++
@@ -1,9 +0,0 @@
-// PRECISIONありのshareに変換します。
-export const formatWithPrecision = (share: string, precision: string) => {
-  return BigInt(share) * BigInt(precision);
-};
-
-// PRECISIONなしのshareに変換します。
-export const formatWithoutPrecision = (share: string, precision: string) => {
-  return (BigInt(share) / BigInt(precision)).toString();
-};
